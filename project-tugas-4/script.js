f5695abb31902625a4b166e51b263e5122663152
irfannahmad <hidayatirfanahmad7@gmail.com>
2023-10-23 18:30:24 +0700

    menyimpan koe latihan

--- project-tugas-4/script.js
+++ project-tugas-4/script.js
@@ -10,4 +10,32 @@ form.addEventListener("submit", (e) => {
    validateInputs();
 });
 
-const validateInputs = () => {};
+const setError = (element, message) => {
+   const inputControl = element.parentElement;
+   const errorDisplay = inputControl.querySelector(".error");
+
+   errorDisplay.innerText = message;
+   inputControl.classList.add("succes");
+   inputControl.classList.remove("error");
+};
+
+const isValidEmail = (email) => {};
+
+const validateInputs = () => {
+   const usernameValue = username.value.trim();
+   const emailValue = email.value.trim();
+   const passwordValue = password.value.trim();
+   const password2Value = password2.value.trim();
+
+   if (usernameValue === "") {
+      setError(username, "Tolong tuliskan username anda!");
+   } else {
+      setSucces(username);
+   }
+
+   if (emailValue === "") {
+      setError(email, "Tolong tuliskan email anda!");
+   } else if (!isValidEmail(emailValue)) {
+      setError(email, "Berikan");
+   }
+};
