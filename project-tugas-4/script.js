11a29f1e94aff3339e1bfbb582b628355dd38f64
irfannahmad <hidayatirfanahmad7@gmail.com>
2023-10-31 11:30:48 +0700

    mengubah kode js ke jquery di file script.js

--- project-tugas-4/script.js
+++ project-tugas-4/script.js
@@ -1,48 +1,43 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const alamat = document.getElementById("alamat");
-const umur = document.getElementById("umur");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+$(document).ready(function () {
+   $("#form").submit(function (event) {
+      event.preventDefault();
 
-form.addEventListener("submit", function (event) {
-   event.preventDefault();
+      var usernameValue = $("#username").val();
+      var alamatValue = $("#alamat").val();
+      var umurValue = $("#umur").val();
+      var emailValue = $("#email").val();
+      var passwordValue = $("#password").val();
+      var password2Value = $("#password2").val();
 
-   if (username.value === "") {
-      alert("Silahkan masukkan username anda!");
-      return;
-   }
+      if (usernameValue === "") {
+         alert("Silahkan masukkan username anda!");
+         return;
+      }
 
-   const emailValue = email.value;
-   const emailPattern = /\S+@\S+\.\S+/;
+      var emailPattern = /\S+@\S+\.\S+/;
 
-   if (!emailPattern.test(emailValue)) {
-      alert("Mohon masukkan alamat email yang valid!");
-      return;
-   }
+      if (!emailPattern.test(emailValue)) {
+         alert("Mohon masukkan alamat email yang valid!");
+         return;
+      }
 
-   if (email.value === "") {
-      alert("Silahkan masukkan email anda!");
-      return;
-   }
+      if (emailValue === "") {
+         alert("Silahkan masukkan email anda!");
+         return;
+      }
 
-   if (password.value === "") {
-      alert("Silahkan masukkan password anda!");
-      return;
-   }
+      if (passwordValue === "") {
+         alert("Silahkan masukkan password anda!");
+         return;
+      }
 
-   if (password2.value !== password.value) {
-      alert("Konfirmasi password harus sama dengan password sebelumnya!");
-      return;
-   }
+      if (password2Value !== passwordValue) {
+         alert("Konfirmasi password harus sama dengan password sebelumnya!");
+         return;
+      }
 
-   alert("Data berhasil ditambahkan");
+      alert("Data berhasil ditambahkan");
 
-   username.value = "";
-   alamat.value = "";
-   umur.value = "";
-   email.value = "";
-   password.value = "";
-   password2.value = "";
+      $("#username, #alamat, #umur, #email, #password, #password2").val("");
+   });
 });
